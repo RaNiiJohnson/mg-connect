c1cfc8baf080f9e49b84e8ed839e8dbc3f708b3a
RaNiiJohnson <johnsontolotriniavo@gmail.com>
2025-11-01 20:14:01 +0300

    change font

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,13 +1,13 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Kantumruy_Pro, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/components/theme-provider";
 import Header from "@/components/header";
 import Footer from "@/components/footer";
 import { Toaster } from "sonner";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+const kantumruyPro = Kantumruy_Pro({
+  variable: "--font-kantumruy-pro",
   subsets: ["latin"],
 });
 
@@ -30,7 +30,7 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${kantumruyPro.variable} ${geistMono.variable} antialiased`}
       >
         <ThemeProvider
           attribute="class"
