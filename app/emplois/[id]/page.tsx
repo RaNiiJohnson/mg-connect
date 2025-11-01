c658ebc1f5092e9c91c3d57abcdc34f8ee268f41
RaNiiJohnson <johnsontolotriniavo@gmail.com>
2025-11-01 22:53:09 +0300

    add city details

--- app/emplois/[id]/page.tsx
+++ app/emplois/[id]/page.tsx
@@ -66,7 +66,7 @@ export default async function JobDetailsPage(props: Pageprops) {
                   </div>
                   <div className="flex items-center gap-2">
                     <MapPin className="h-4 w-4 text-muted-foreground" />
-                    <span>{jobOffer.city}</span>
+                    <span>Ville : {jobOffer.city}</span>
                   </div>
                   <div className="flex items-center gap-2">
                     <Calendar className="h-4 w-4 text-muted-foreground" />
